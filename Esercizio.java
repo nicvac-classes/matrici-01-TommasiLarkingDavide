61f53b474c370b0b81ab74adf753ada57d56b73e
TommasiLarkingDavide <davide.tommasilarking@marconibari.edu.it>
2025-11-04 11:02:55 +0000

    ConsegnaTommasi

--- Esercizio.java
+++ Esercizio.java
@@ -10,60 +10,30 @@ public class Esercizio {
     M[1][3] = 7;
     M[1][4] = 7;
 
-    UtilsMatrice.visualizza(Matrice);
+    UtilsMatrice.visualizza(M);
 }
+     public static void esercizio2() {
+   
+    int j;
+    int [][] M = new int[3][5];
+    for (j=0; j<=4; j=j+1 ) {
+        M[1][j] = 7;
     }
-   public static void riempiPerRighe(/* scrivere qui i parametri formali */) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
-    }
-    public static void riempiRighe(int [][] M, int righe,int  colonne) {
-		int i,c,j;
-		c=1;
-		i=0;
-		while(i<=righe-1) {
-			j=0;
-			while(j<colonne-1) {
-				M[i][j]=c;
-				++c;
-				++j;
-			}
-			++i;
-		}
-	}
 
-    public static void riempiPerColonne(/* scrivere qui i parametri formali */) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
-    }
-  	public static void riempiColonne(int [][] M, int righe,int  colonne) {
-		int i,c,j;
-		c=1;
-		j=0;
-		while(j<=colonne-1) {
-			i=0;
-			while(i<righe-1) {
-				M[i][j]=c;
-				++c;
-				++i;
-			}
-			++j;
-		}
-	}
-    
-    public static void main(String[] args) {
+    UtilsMatrice.visualizza(M);
 
-        System.out.println();
-        System.out.println("ESERCIZIO 1 davide: ");
-        System.out.println("----- ----- -----");
-        esercizio1();
-        
-        System.out.println();
-        System.out.println("ESERCIZIO 2: ");
-        System.out.println("----- ----- -----");
-        esercizio2();
-        
-        System.out.println();
-        System.out.println("ESERCIZIO 3: ");
-        System.out.println("----- ----- -----");
-        esercizio3();
+
+}
+
+public static void esercizio3() {
+   
+    int i;
+    int [][] M = new int[3][5];
+    for (i=0; i<=4; i=i+1 ) {
+        M[i][3] = 7;
     }
+
+    UtilsMatrice.visualizza(M);
+
+}
 }
