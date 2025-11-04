bfdfe3bab92b77690cd215ce84735da89d163d1b
TommasiLarkingDavide <davide.tommasilarking@marconibari.edu.it>
2025-11-04 10:52:44 +0000

    CONSEGNA2

--- Esercizio.java
+++ Esercizio.java
@@ -2,16 +2,52 @@
 public class Esercizio {
     
     public static void esercizio1() {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO 1
+        public static void esercizio1() {
+    int [][] M = new int[3][5];
+    M[1][0] = 7;
+    M[1][1] = 7;
+    M[1][2] = 7;
+    M[1][3] = 7;
+    M[1][4] = 7;
+
+    UtilsMatrice.visualizza(Matrice);
+}
     }
-    
-    public static void esercizio2() {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO 2
+   public static void riempiPerRighe(/* scrivere qui i parametri formali */) {
+        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
     }
-    
-    public static void esercizio3() {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO 3
+    public static void riempiRighe(int [][] M, int righe,int  colonne) {
+		int i,c,j;
+		c=1;
+		i=0;
+		while(i<=righe-1) {
+			j=0;
+			while(j<colonne-1) {
+				M[i][j]=c;
+				++c;
+				++j;
+			}
+			++i;
+		}
+	}
+
+    public static void riempiPerColonne(/* scrivere qui i parametri formali */) {
+        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
     }
+  	public static void riempiColonne(int [][] M, int righe,int  colonne) {
+		int i,c,j;
+		c=1;
+		j=0;
+		while(j<=colonne-1) {
+			i=0;
+			while(i<righe-1) {
+				M[i][j]=c;
+				++c;
+				++i;
+			}
+			++j;
+		}
+	}
     
     public static void main(String[] args) {
 
